0565e2ae3de5a8a7b01a38b169ca9110ba7e5ad0
lucas <lucasbezerra.rocinha@gmail.com>
2023-03-17 18:14:23 -0300

    Adicionando funcionalidades no componente admin

--- src/Pages/Admin/index.js
+++ src/Pages/Admin/index.js
@@ -1,9 +1,45 @@
 import './style.css'
+import { useState } from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../firebase';
 
 export default function Admin() {
-    return(
-        <div>
-            <h1>ADMIN</h1>
+
+    const [tarefa, setTarefa] = useState('')
+
+    function handleRegister(e) {
+        e.preventDefault();
+
+        alert('clicou')
+    }
+
+    async function handleLogout() {
+        await signOut(auth)
+    }
+
+    return (
+        <div className="admin-container">
+            <h1>Minhas tarefas</h1>
+
+            <form className='form' onSubmit={handleRegister}>
+                <textarea
+                    placeholder="Digite sua tarefa..."
+                    value={tarefa}
+                    onChange={event => setTarefa(event.target.value)}
+                />
+
+                <button className='btn-register' type="submit">Registrar tarefa</button>
+            </form>
+
+            <article className='list'>
+                <p>TAREFAS</p>
+                <div>
+                    <button>Editar</button>
+                    <button className='btn-delete'>Concluir</button>
+                </div>
+            </article>
+
+            <button className='btn-logout' onClick={handleLogout}>Sair</button>
         </div>
     )
 }
